b762dbb31c1561b9fbba5234e60f371ac7f83599
peterlimpens <plimpens@icloud.com>
2022-06-16 09:45:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<TheNavigation></TheNavigation>
+	<div class="container">
+		<router-view :key="$route.path"></router-view>
+	</div>
+</template>
+
+<script>
+	import TheNavigation from "@/components/TheNavigation.vue";
+	export default {
+		components: {TheNavigation}
+	}
+
+</script>
---
+++ src/views/DestinationShow.vue
@@ -0,0 +1,26 @@
+<template>
+	<section  v-if="destination" class="destination">
+		<h1>{{destination.name}}</h1>
+		<div class="destination-details">
+			<img :src="`/images/${destination.image}`" :alt="destination.name">
+			<p>{{destination.description}}</p>
+		</div>
+	</section	>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+	name: "DestinationShow",
+	
+	computed: {
+		destinationId() {
+			return parseInt(this.$route.params.id)
+		},
+		destination() {
+			return sourceData.destinations.find(destination =>  destination.id === this.destinationId)
+		}
+	},
+}
+</script>
