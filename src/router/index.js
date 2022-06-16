83f904f71fab41ec67acc95cec0545f00e6c4b91
peterlimpens <plimpens@icloud.com>
2022-06-16 14:29:17 +0200

    router guards added

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory, useRoute} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '@/views/Home.vue'
 import sourceData from '@/data.json'
 
@@ -6,12 +6,30 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: Home},
+        component: Home
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: () => import('@/views/NotFound.vue'),
+    },
     {
         path: '/destination/:id/:slug',
         name: 'destination.show',
         component: ()=>import('@/views/DestinationShow.vue'),
         props: route => ({...route.params, id: parseInt(route.params.id)}),
+        beforeEnter(to, from){
+            const exists = sourceData.destinations.find(
+                destination => destination.id === parseInt(to.params.id)
+            )
+            if(!exists) return {
+                name: 'NotFound',
+                // allows keeping the URL while rendering a different page
+                params: { pathMatch: to.path.split('/').slice(1) },
+                query: to.query,
+                hash: to.hash,
+            }
+        },
         children: [
             {
                 path: ':experienceSlug',
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,11 @@
 <template>
-
+	<div>
+		<h1>Not Found</h1>
+		<p>
+			Oops, we could not find this page! Try going
+			<router-link to="/">home</router-link>
+		</p>
+	</div>
 </template>
 
 <script>
